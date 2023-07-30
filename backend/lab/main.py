f6da8abb45886074e32bb3fa62fced41c547ed8b
maria <pinina7773@gmail.com>
2023-07-30 09:04:38 +0300

    add photos saving

--- backend/lab/main.py
+++ backend/lab/main.py
@@ -1,11 +1,14 @@
+import base64
+import json
+from pathlib import Path
+
 import redis
 from fastapi import UploadFile, HTTPException, FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import StreamingResponse
-
-from lab.models import Photo
-from tasks import process_photo
+from .tasks import process_photo
 import io
+
 
 app = FastAPI()
 
@@ -16,6 +19,8 @@ app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+UPLOAD_DIR = Path("photos")
+
 
 @app.post("/upload/")
 async def upload_photo(file: UploadFile, user_id: int = 1):
@@ -24,10 +29,21 @@ async def upload_photo(file: UploadFile, user_id: int = 1):
 
             photo_bytes = await file.read()
 
-            task_result = process_photo.delay(photo_bytes, user_id)
+            result = process_photo.delay(photo_bytes, user_id)
+            task_result, processed_photo_bytes = result.get()
+
+            original_photo_path = UPLOAD_DIR / f"{task_result['id']}_{task_result['timestamp']}_original.jpg"
+            processed_photo_path = UPLOAD_DIR / f"{task_result['id']}__{task_result['timestamp']}_processed.jpg"
+
+            UPLOAD_DIR.mkdir(exist_ok=True)
+            with original_photo_path.open("wb") as f:
+                f.write(photo_bytes)
+
+            with processed_photo_path.open("wb") as f:
+                f.write(processed_photo_bytes)
 
             return {"message": "Фотография успешно загружена и поставлена в очередь на обработку.",
-                    "task_id": task_result.id}
+                    "task_id": task_result['id']}
 
         else:
             raise HTTPException(status_code=400, detail="Загруженный файл не является изображением.")
@@ -43,7 +59,10 @@ async def get_result(task_id: str):
 
         if task_result.ready():
             if task_result.successful():
-                # processed_photo_bytes = task_result.get()
+                photo_path = UPLOAD_DIR / f"{task_id}_processed.jpg"
+
+                with photo_path.open("rb") as f:
+                    processed_photo_bytes = f.read()
 
                 return {"message": "Фотография успешно обработана.", "download_link": f"/download/{task_id}"}
 
@@ -63,7 +82,10 @@ async def download_result(task_id: str):
         task_result = process_photo.AsyncResult(task_id)
 
         if task_result.ready() and task_result.successful():
-            processed_photo_bytes = task_result.get()
+            photo_path = UPLOAD_DIR / f"{task_id}_processed.jpg"
+
+            with photo_path.open("rb") as f:
+                processed_photo_bytes = f.read()
 
             return StreamingResponse(io.BytesIO(processed_photo_bytes), media_type="image/jpeg",
                                      headers={"Content-Disposition": f"attachment; filename=processed_{task_id}.jpg"})
@@ -78,10 +100,16 @@ async def download_result(task_id: str):
 @app.get("/photos/processed/")
 async def get_processed_photos():
     try:
-        photo_keys = redis_client.keys('photo:*')
-        processed_photos = [Photo.parse_raw(redis_client.get(key)) for key in photo_keys]
+        processed_photos = []
+        for photo_path in UPLOAD_DIR.glob("*_processed.jpg"):
+            photo_id = photo_path.stem.split('_')[0]
+            timestamp = photo_path.stem.split('_')[-2]
+            with photo_path.open("rb") as f:
+                photo_bytes = f.read()
+                photo_base64 = base64.b64encode(photo_bytes).decode('utf-8')
+                processed_photos.append({"id": int(photo_id), "photo_bytes": photo_base64, 'timestamp': timestamp})
 
-        return processed_photos
+        return json.dumps(processed_photos)
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Произошла ошибка: {e}")
--- backend/lab/tasks.py
+++ backend/lab/tasks.py
@@ -1,19 +1,19 @@
+import json
 from datetime import datetime
+
 from PIL import Image
 from io import BytesIO
-import base64
-from celery_worker import celery_app
-
 import redis
 
 from lab.models import Photo
+from celery_worker import celery_app
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
 # Apply a simple black-and-white filter
 @celery_app.task
-def process_photo(user_id, photo_bytes):
+def process_photo(photo_bytes, user_id):
     try:
         image = Image.open(BytesIO(photo_bytes))
 
@@ -23,27 +23,17 @@ def process_photo(user_id, photo_bytes):
         bw_image.save(output_buffer, format='JPEG')
         processed_photo_bytes = output_buffer.getvalue()
 
-        processed_photo_base64 = base64.b64encode(processed_photo_bytes).decode('utf-8')
-
         photo_id = redis_client.incr('photo_id')
         original_photo_url = f'/photos/original/{photo_id}.jpg'
         processed_photo_url = f'/photos/processed/{photo_id}.jpg'
         timestamp = str(datetime.now())
 
-        photo = Photo(
-            id=photo_id,
-            user_id=user_id,
-            original_photo_url=original_photo_url,
-            processed_photo_url=processed_photo_url,
-            timestamp=timestamp,
-            processed_photo_base64=processed_photo_base64
-        )
-        redis_client.set(f'photo:{photo_id}', photo.model_dump_json())
+        photo = Photo(id=photo_id, user_id=user_id, original_photo_url=original_photo_url,
+                      processed_photo_url=processed_photo_url, timestamp=timestamp, )
+        photo_dict = photo.to_dict()
+        redis_client.set(f'photo:{photo_id}', json.dumps(photo_dict))
 
-        import time
-        time.sleep(5)
-
-        return photo
+        return photo.to_dict(), processed_photo_bytes
 
     except Exception as e:
         raise e
