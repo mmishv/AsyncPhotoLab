751bef5282a4ed3e664f468c2019f59a7715f1cd
maria <pinina7773@gmail.com>
2023-07-30 09:12:56 +0300

    style photo list page

--- frontend/src/components/PhotoList.js
+++ frontend/src/components/PhotoList.js
@@ -1,16 +1,28 @@
-import React, {useState} from 'react';
-
+import React, {useEffect, useState} from 'react';
+import '../styles/PhotoList.css';
 
 const PhotoList = () => {
     const [photos, setPhotos] = useState([]);
-    return (<div>
+
+    useEffect(() => {
+        fetch('/photos/processed/')
+            .then(response => response.json())
+            .then(data => setPhotos(JSON.parse(data)))
+            .catch(error => console.error('Error:', error));
+    }, []);
+
+    return (<div className="photo-list">
+        <div className="photo-list-header">
             <h2>Обработанные фотографии</h2>
-            <ul>
-                {photos.map((photo) => (<li key={photo.id}>
-                        <img src={photo.url} alt={`Processed-${photo.id}`}/>
-                    </li>))}
-            </ul>
-        </div>);
+            <a href="/" className="home-link">На главную</a>
+        </div>
+        <div className="photo-grid">
+            {photos.map((photo) => (<div className="photo-card" key={photo.id}>
+                <img src={`data:image/jpeg;base64,${photo.photo_bytes}`} alt={`Processed-${photo.id}`}/>
+                <div className="date">Дата добавления: {photo.timestamp}</div>
+            </div>))}
+        </div>
+    </div>);
 };
 
 export default PhotoList;
